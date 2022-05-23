149c4f5f94793cc522a311df10329b9060fc819a
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2022-05-23 08:08:27 +0100

    adding gitlab kubernetes agent config

--- cdk/fetchNews/lambda_function.py
+++ cdk/fetchNews/lambda_function.py
@@ -1,19 +1,32 @@
+import json
+import os
+
 import boto3
 from boto3.dynamodb.conditions import Key
-#Best practice is to create the boto3 connections in the global section
-#and not inside the handler - More on this in the future lecturees
+
+# Best practice is to create the boto3 connections in the global section
+# and not inside the handler - More on this in the future lecturees
 dynamodb = boto3.resource('dynamodb')
+TABLE_NAME = os.environ.get("TABLE_NAME")
+
 
 def lambda_handler(event, context):
-    
-    table = dynamodb.Table('newsTable')
-    inputSentiment=event['sentiment']
+    table = dynamodb.Table(TABLE_NAME)
+    # body = json.loads(event['body'])
+    inputSentiment = event['sentiment']
     try:
         # Querying the table using Primary key
         response = table.query(
             KeyConditionExpression=Key('sentiment').eq(inputSentiment),
-            Limit=10, #limits returned news to 10
-            ScanIndexForward=False) #descending order of timestamp, most recent news first
-        return response
+            Limit=10,  # limits returned news to 10
+            ScanIndexForward=False)  # descending order of timestamp, most recent news first
+        return {
+            "isBase64Encoded": "false",
+            "statusCode": 200,
+            "body": json.dumps(response),
+            "headers": {
+                "content-type": "application/json"
+            }
+        }
     except:
         raise
--- cdk/getNews/getNews.py
+++ cdk/getNews/getNews.py
@@ -4,72 +4,90 @@ import json
 import datetime
 import os
 import boto3
+import uuid
 
 API_KEY = os.environ.get('API_KEY')
 TABLE_NAME = os.environ.get("TABLE_NAME")
 
-#this lambda grabs today's headlines, does sentiment analysis using AWS Comprehend
-#and saves the news along with sentiment into a dynamodb table
+
+# this lambda grabs today's headlines, does sentiment analysis using AWS Comprehend
+# and saves the news along with sentiment into a dynamodb table
 def lambda_handler(event, context):
     # TODO implement
     # print("button pressed")
-    print(event)
-    print("just changing a print")
-    if event['action']=='insert news':
+
+    if event['action'] == 'insert news':
         findNews()
-    elif  event['action']=='delete news':
+    elif event['action'] == 'delete news':
         deleteNews()
 
-    return 'End of News Sentiment IOT function'
-    
+    return {
+        "isBase64Encoded": "false",
+        "statusCode": 200,
+        "body": "Success",
+        "headers": {
+            "content-type": "application/json"
+        }
+    }
 
 
 def deleteNews():
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TABLE_NAME)
-    #Scanning the table to get all rows in one shot
-    response =table.scan()
+    # Scanning the table to get all rows in one shot
+    response = table.scan()
     if 'Items' in response:
-        items=response['Items']
+        items = response['Items']
         print(f"item number: {len(items)}")
         for row in items:
-            sentiment=row['sentiment']
-            timestamp=row['time']
+            sentiment = row['sentiment']
+            timestamp = row['time']
             delresponse = table.delete_item(
                 Key={
-                'sentiment': sentiment,
-                'time':timestamp
-                    }
-                    )
+                    'sentiment': sentiment,
+                    'time': timestamp
+                }
+            )
+
 
 def findNews():
-    #News credit to newsapi.org
-    #Fetch headlines using the API
-    #IMPORTANT: Register in newsapi.org to get your own API key, it's super easy!
+    # News credit to newsapi.org
+    # Fetch headlines using the API
     response = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={API_KEY}")
-    d=response.json()
+    d = response.json()
     if (d['status']) == 'ok':
         for article in d['articles']:
-            newsTitle=article['title']
-            timestamp=article['publishedAt']
-            sentiment=json.loads(getSentiment(newsTitle))
+            id = str(uuid.uuid4())
+            newsTitle = article['title']
+            timestamp = article['publishedAt']
+            content = article['content']
+            image = article['urlToImage']
+            sentiment = json.loads(getSentiment(newsTitle, content))
             print(f"Sentiment: {sentiment}")
-            insertDynamo(sentiment['Sentiment'],newsTitle,timestamp)
+            insertDynamo(sentiment['Sentiment'], newsTitle, timestamp, content, image, id)
 
-#getSentiment function calls AWS Comprehend to get the sentiment
-def getSentiment(newsTitle):
+
+# getSentiment function calls AWS Comprehend to get the sentiment
+def getSentiment(newsTitle, content):
+    text = newsTitle
+    if content:
+        text = text + content
     comprehend = boto3.client(service_name='comprehend')
-    return(json.dumps(comprehend.detect_sentiment(Text=newsTitle, LanguageCode='en'), sort_keys=True))
+    return json.dumps(comprehend.detect_sentiment(Text=text, LanguageCode='en'), sort_keys=True)
 
-#inserts headline along with sentiment into Dynamo
-def insertDynamo(sentiment,newsTitle,timestamp):
+
+# inserts headline along with sentiment into Dynamo
+def insertDynamo(sentiment, newsTitle, timestamp, content, image,id):
     print("inside insert dynamo function")
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table(TABLE_NAME)
     response = table.put_item(
-       Item={
-        'sentiment': sentiment, 
-        'title': newsTitle,
-        'time' : timestamp
-       }
-       )
+        Item={
+            "id":id,
+            'sentiment': sentiment,
+            'title': newsTitle,
+            'time': timestamp,
+            'content': content,
+            'image': image
+        }
+    )
